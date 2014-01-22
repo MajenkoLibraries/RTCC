79ea63ce2cfa7d50f464ad2b0c0d027ff860b6d9
Matt Jenkins <matt@majenko.co.uk>
2014-01-22 14:06:42 +0000

    Fixes by Christophe Dupriez

--- RTCC.cpp
+++ RTCC.cpp
@@ -1,17 +1,41 @@
 /*
-
-All code is copyright 2011 Majenko Technologies. 
-The code is provided freely with no warranty either implicitly or explicity implied. 
-You may freely use the code for whatever you wish, but Majenko Technologies will not 
-be liable for any damage of any form howsoever caused by the use or misuse of this code.
-
-*/
+ * Copyright (c) 2013, Majenko Technologies
+ * All rights reserved.
+ *
+ * Additions (c) 2014, Christophe Dupriez, DESTIN-Informatique.com
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <WProgram.h>
 #include <sys/attribs.h>
+#include <peripheral/int.h>
 #include "RTCC.h"
 
-RTCCClass RTCC;
+RTCCClass RTCC; // Object to access the RTC of the MCU...
 
 extern "C"
 {
@@ -27,6 +51,9 @@ extern "C"
     }
 }
 
+#define UNLOCK SYSKEY = 0x0; SYSKEY = 0xAA996655; SYSKEY = 0x556699AA;
+#define LOCK SYSKEY = 0x0;
+
 void RTCCClass::begin() 
 {
     IEC1CLR=0x00008000;
@@ -34,139 +61,79 @@ void RTCCClass::begin()
     IPC8CLR=0x1f000000; 
     IPC8SET=0x0d000000; 
     IEC1SET=0x00008000; 
-
-    unlock();
-    OSCCONSET = 2;
-    RTCCONbits.ON = 1;
+/*Ensure the secondary oscillator is enabled and ready, i.e. OSCCON<1>=1, OSCCON<22>=1,
+and RTCC write is enabled i.e. RTCWREN (RTCCON<3>) =1;*/
+    UNLOCK
     RTCCONbits.RTCWREN = 1;
-    lock();
+    RTCCONbits.ON = 1;
+    OSCCONSET = 0x00400002;
+    LOCK
 }
 
-void RTCCClass::unlock()
-{
-    SYSKEY = 0x0;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;
-}
 
-void RTCCClass::lock()
-{
-    SYSKEY = 0x0;
-}
-
+/*
+ * Wait at least 32msec... if a crystal is available!
+ * FOREVER IF NO CRYSTAL WORKING...
+ */
 void RTCCClass::timeSync()
 {
-    while(RTCCONbits.RTCSYNC==1);
+/* PROBABLY NOT A GOOD IDEA!
+    while(RTCCONbits.RTCSYNC==0); // Wait for a transition and 32ms later
+*/
+    while(RTCCONbits.RTCSYNC==1) { /*yield*/ };
 }
 
 void RTCCClass::outputEnable()
 {
     timeSync();
-    unlock();
+    UNLOCK
     RTCCONbits.RTCOE = 1;
     RTCCONbits.RTSECSEL = 1;
-    lock();
+    LOCK
 } 
 
 void RTCCClass::outputEnable(unsigned char mode)
 {
     timeSync();
-    unlock();
+    UNLOCK
     RTCCONbits.RTCOE = 1;
     RTCCONbits.RTSECSEL = mode;
-    lock();
+    LOCK
 } 
 
 void RTCCClass::outputDisable()
 {
     timeSync();
-    unlock();
+    UNLOCK
     RTCCONbits.RTCOE = 0;
-    lock();
+    LOCK
 } 
 
-unsigned char RTCCClass::hours()
+RTCCValue RTCCClass::value()
 {
     timeSync();
-    return bcd2dec((RTCTIME >> 24) & 0xFF);
+	uint32_t our_date = RTCDATE;
+	uint32_t our_time = RTCTIME;
+	while (1) {
+		uint32_t new_date = RTCDATE;
+		uint32_t new_time = RTCTIME;
+		if (new_date == our_date && new_time == our_time) break;
+		our_date = new_date;
+		our_time = new_time;
+	}
+	return RTCCValue(our_date,our_time);
 }
 
-void RTCCClass::hours(unsigned char hours)
+void RTCCClass::set(RTCCValue val)
 {
     timeSync();
-    RTCTIME = (RTCTIME & 0x00FFFFFF) | (dec2bcd(hours)<<24);
+	RTCDATE = val.date();
+	RTCTIME = val.time();
 }
 
-unsigned char RTCCClass::minutes()
+void RTCCClass::alarmSync()
 {
-    timeSync();
-    return bcd2dec((RTCTIME >> 16) & 0xFF);
-}
-
-void RTCCClass::minutes(unsigned char minutes)
-{
-    timeSync();
-    RTCTIME = (RTCTIME & 0xFF00FFFF) | (dec2bcd(minutes)<<16);
-}
-
-unsigned char RTCCClass::seconds()
-{
-    timeSync();
-    return bcd2dec((RTCTIME >> 8) & 0xFF);
-}
-
-void RTCCClass::seconds(unsigned char seconds)
-{
-    timeSync();
-    RTCTIME = (RTCTIME & 0xFFFF00FF) | (dec2bcd(seconds)<<8);
-}
-
-unsigned char RTCCClass::year()
-{
-    timeSync();
-    return bcd2dec((RTCDATE >> 24) & 0xFF);
-}
-
-void RTCCClass::year(unsigned char year)
-{
-    timeSync();
-    RTCDATE = (RTCDATE & 0x00FFFFFF) | (dec2bcd(year)<<24);
-}
-
-unsigned char RTCCClass::month()
-{
-    timeSync();
-    return bcd2dec((RTCDATE >> 16) & 0xFF);
-}
-
-void RTCCClass::month(unsigned char month)
-{
-    timeSync();
-    RTCDATE = (RTCDATE & 0xFF00FFFF) | (dec2bcd(month)<<16);
-}
-
-unsigned char RTCCClass::day()
-{
-    timeSync();
-    return bcd2dec((RTCDATE >> 8) & 0xFF);
-}
-
-void RTCCClass::day(unsigned char day)
-{
-    timeSync();
-    RTCDATE = (RTCDATE & 0xFFFF00FF) | (dec2bcd(day)<<8);
-}
-
-unsigned char RTCCClass::dayOfWeek()
-{
-    timeSync();
-    return bcd2dec(RTCDATE & 0xFF);
-}
-
-void RTCCClass::dayOfWeek(unsigned char dow)
-{
-    timeSync();
-    RTCDATE = (RTCDATE & 0xFFFFFF00) | dec2bcd(dow);
+    while(RTCALRMbits.ALRMSYNC==1) { /*yield*/ };
 }
 
 void RTCCClass::alarmEnable()
@@ -205,12 +172,29 @@ void RTCCClass::alarmRepeat(unsigned char rpt)
     RTCALRMbits.ARPT = rpt;
 }
 
-void RTCCClass::alarmSync()
-{
-    while(RTCALRMbits.ALRMSYNC==1);
+void RTCCClass::alarmSet(RTCCValue val) {
+	alarmSync();
+	ALRMDATE = val.date();
+	ALRMTIME = val.time();
 }
 
-unsigned char RTCCClass::dec2bcd(unsigned char decimal)
+RTCCValue RTCCClass::alarmValue() {
+	alarmSync();
+	return RTCCValue(ALRMDATE,ALRMTIME);
+}
+
+void RTCCClass::attachInterrupt(void (*i)())
+{
+    _RTCCInterrupt = i;
+}
+
+/* Handling a date/time : current RTC, Alarm, logged event, etc. */
+RTCCValue::RTCCValue(uint32_t date, uint32_t time) {
+	a_date = date;
+	a_time = time;
+}
+
+unsigned char RTCCValue::dec2bcd(unsigned char decimal)
 {
     unsigned char bcd;
     unsigned char tens,units;
@@ -220,7 +204,7 @@ unsigned char RTCCClass::dec2bcd(unsigned char decimal)
     return bcd;
 }
 
-unsigned char RTCCClass::bcd2dec(unsigned char bcd)
+unsigned char RTCCValue::bcd2dec(unsigned char bcd)
 {
     unsigned char decimal;
     unsigned char tens,units;
@@ -230,45 +214,149 @@ unsigned char RTCCClass::bcd2dec(unsigned char bcd)
     return decimal;
 }
 
-void RTCCClass::alarmHours(unsigned char hours)
+void RTCCValue::bcd2str(unsigned char * res, unsigned char bcd)
 {
-    alarmSync();
-    ALRMTIME = (ALRMTIME & 0x00FFFFFF) | (dec2bcd(hours)<<24);
+    unsigned char tens,units;
+    tens = bcd >> 4;
+    units = bcd & 0x0F;
+    res[0] = tens +'0';
+	res[1] = units+ '0';
 }
 
-void RTCCClass::alarmMinutes(unsigned char minutes)
+uint32_t RTCCValue::date()
 {
-    alarmSync();
-    ALRMTIME = (ALRMTIME & 0xFF00FFFF) | (dec2bcd(minutes)<<16);
+	return a_date;
 }
 
-void RTCCClass::alarmSeconds(unsigned char seconds)
+uint32_t RTCCValue::time()
 {
-    alarmSync();
-    ALRMTIME = (ALRMTIME & 0xFFFF00FF) | (dec2bcd(seconds)<<8);
+	return a_time;
 }
 
-void RTCCClass::alarmMonth(unsigned char month)
+// sizeof res must be 9 or bigger
+void RTCCValue::date(unsigned char * res)
 {
-    alarmSync();
-    ALRMDATE = (ALRMDATE & 0xFF00FFFF) | (dec2bcd(month)<<16);
+	unsigned char ss[2];
+	bcd2str(ss, (a_date >> 24) & 0xFF);
+	res[0] = ss[0];
+	res[1] = ss[1];
+	res[2] = '-';
+	bcd2str(ss, (a_date >> 16) & 0xFF);
+	res[3] = ss[0];
+	res[4] = ss[1];
+	res[5] = '-';
+	bcd2str(ss, (a_date >> 8) & 0xFF);
+	res[6] = ss[0];
+	res[7] = ss[1];
+	res[8] = 0;
 }
 
-void RTCCClass::alarmDay(unsigned char day)
+// sizeof res must be 9 or bigger
+void RTCCValue::time(unsigned char * res)
 {
-    alarmSync();
-    ALRMDATE = (ALRMDATE & 0xFFFF00FF) | (dec2bcd(day)<<8);
+	unsigned char ss[2];
+	bcd2str(ss, (a_time >> 24) & 0xFF);
+	res[0] = ss[0];
+	res[1] = ss[1];
+	res[2] = ':';
+	bcd2str(ss, (a_time >> 16) & 0xFF);
+	res[3] = ss[0];
+	res[4] = ss[1];
+	res[5] = ':';
+	bcd2str(ss, (a_time >> 8) & 0xFF);
+	res[6] = ss[0];
+	res[7] = ss[1];
+	res[8] = 0;
 }
 
-void RTCCClass::alarmDayOfWeek(unsigned char dow)
+unsigned char RTCCValue::year()
 {
-    alarmSync();
-    ALRMDATE = (ALRMDATE & 0xFFFFFF00) | dec2bcd(dow);
+    return bcd2dec((a_date >> 24) & 0xFF);
 }
 
-void RTCCClass::attachInterrupt(void (*i)())
+unsigned char RTCCValue::month()
 {
-    _RTCCInterrupt = i;
+    return bcd2dec((a_date >> 16) & 0xFF);
+}
+
+unsigned char RTCCValue::day()
+{
+    return bcd2dec((a_date >> 8) & 0xFF);
+}
+
+unsigned char RTCCValue::dayOfWeek()
+{
+    return bcd2dec(a_date & 0xFF);
+}
+
+unsigned char RTCCValue::hours()
+{
+    return bcd2dec((a_time >> 24) & 0xFF);
 }
 
 
+unsigned char RTCCValue::minutes()
+{
+    return bcd2dec((a_time >> 16) & 0xFF);
+}
+
+unsigned char RTCCValue::seconds()
+{
+    return bcd2dec((a_time >> 8) & 0xFF);
+}
+
+void RTCCValue::time(unsigned char hours, unsigned char minutes, unsigned char seconds)
+{
+    a_time = (a_time & 0xFF) | (dec2bcd(hours)<<24) | (dec2bcd(minutes)<<16) | (dec2bcd(seconds)<<8);
+}
+
+void RTCCValue::hours(unsigned char hours)
+{
+    a_time = (a_time & 0x00FFFFFF) | (dec2bcd(hours)<<24);
+}
+void RTCCValue::minutes(unsigned char minutes)
+{
+    a_time = (a_time & 0xFF00FFFF) | (dec2bcd(minutes)<<16);
+}
+
+void RTCCValue::seconds(unsigned char seconds)
+{
+    a_time = (a_time & 0xFFFF00FF) | (dec2bcd(seconds)<<8);
+}
+
+void RTCCValue::date(unsigned char year, unsigned char month, unsigned char day)
+{
+    a_date = (a_date & 0xFF) | (dec2bcd(year)<<24) | (dec2bcd(month)<<16) | (dec2bcd(day)<<8);
+}
+
+void RTCCValue::year(unsigned char year)
+{
+    a_date = (a_date & 0x00FFFFFF) | (dec2bcd(year)<<24);
+}
+
+void RTCCValue::month(unsigned char month)
+{
+    a_date = (a_date & 0xFF00FFFF) | (dec2bcd(month)<<16);
+}
+
+void RTCCValue::day(unsigned char day)
+{
+    a_date = (a_date & 0xFFFF00FF) | (dec2bcd(day)<<8);
+}
+
+void RTCCValue::dayOfWeek(unsigned char dow)
+{
+    a_date = (a_date & 0xFFFFFF00) | dec2bcd(dow);
+}
+
+bool RTCCValue::valid()
+{
+   char ayear  = year();
+   char amonth = month();
+   char aday   = day();
+   char ahour  = hours();
+   char amin   = minutes();
+   char asec   = seconds();
+   return (ayear >= 0 && ayear <= 99 && amonth >= 1 && amonth <= 12 && aday >= 1 && aday <= 31
+           && ahour >= 0 && ahour <= 23 && amin >= 0 && amin <= 59 && asec >= 0 && asec <= 59 );
+}
--- RTCC.h
+++ RTCC.h
@@ -1,11 +1,34 @@
 /*
-
-All code is copyright 2011 Majenko Technologies. 
-The code is provided freely with no warranty either implicitly or explicity implied. 
-You may freely use the code for whatever you wish, but Majenko Technologies will not 
-be liable for any damage of any form howsoever caused by the use or misuse of this code.
-
-*/
+ * Copyright (c) 2013, Majenko Technologies
+ * All rights reserved.
+ *
+ * Additions (c) 2014, Christophe Dupriez, DESTIN-Informatique.com
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * * Neither the name of Majenko Technologies nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef _RTCC_H_INCLUDED
 #define _RTCC_H_INCLUDED
@@ -24,52 +47,62 @@ be liable for any damage of any form howsoever caused by the use or misuse of th
 #define ALARM           0x0
 #define SECONDS         0x1
 
+class RTCCValue {
+	private:
+		uint32_t a_date;
+		uint32_t a_time;
+		unsigned char bcd2dec(unsigned char);
+		unsigned char dec2bcd(unsigned char);
+		void bcd2str(unsigned char * res, unsigned char bcd);
+	public:
+		RTCCValue(uint32_t date, uint32_t time);
+		uint32_t date();
+		uint32_t time();
+		bool valid();
+		void time(unsigned char hours, unsigned char minutes, unsigned char seconds);
+		void time(unsigned char * res);
+		void date(unsigned char year, unsigned char month, unsigned char day);
+		void date(unsigned char * res);
+		unsigned char hours();
+		void hours(unsigned char);
+		unsigned char minutes();
+		void minutes(unsigned char);
+		unsigned char seconds();
+		void seconds(unsigned char);
+		unsigned char year();
+		void year(unsigned char);
+		unsigned char month();
+		void month(unsigned char);
+		unsigned char day();
+		void day(unsigned char);
+		unsigned char dayOfWeek();
+		void dayOfWeek(unsigned char);
+};
+
 class RTCCClass {
 	public:
 		void begin();
-                unsigned char hours();
-                void hours(unsigned char);
-                unsigned char minutes();
-                void minutes(unsigned char);
-                unsigned char seconds();
-                void seconds(unsigned char);
-                unsigned char year();
-                void year(unsigned char);
-                unsigned char month();
-                void month(unsigned char);
-                unsigned char day();
-                void day(unsigned char);
-                unsigned char dayOfWeek();
-                void dayOfWeek(unsigned char);
+		RTCCValue value();
+		void set(RTCCValue val);
 
-                void outputEnable();
-                void outputEnable(unsigned char);
-                void outputDisable();
+		void outputEnable();
+		void outputEnable(unsigned char);
+		void outputDisable();
 
-                void alarmEnable();
-                void alarmDisable();
-                void chimeEnable();
-                void chimeDisable();
-                void alarmMask(unsigned char);
-                void alarmRepeat(unsigned char);
+		RTCCValue alarmValue();
+		void alarmEnable();
+		void alarmDisable();
+		void chimeEnable();
+		void chimeDisable();
+		void alarmMask(unsigned char);
+		void alarmRepeat(unsigned char);
 
-                void alarmHours(unsigned char);
-                void alarmMinutes(unsigned char);
-                void alarmSeconds(unsigned char);
-                void alarmMonth(unsigned char);
-                void alarmDay(unsigned char);
-                void alarmDayOfWeek(unsigned char);
+		void alarmSet(RTCCValue val);
+		void attachInterrupt(void (*)());
 
-                void attachInterrupt(void (*)());
-
-        private:
-                void unlock();
-                void lock();
-                void timeSync();
-                void alarmSync();
-                unsigned char bcd2dec(unsigned char);
-                unsigned char dec2bcd(unsigned char);
-
+	private:
+		void timeSync();
+		void alarmSync();
 };
 
 extern RTCCClass RTCC;
